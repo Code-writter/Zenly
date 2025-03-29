d562df45228a4791fda09c074e8367f90b81dc32
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-03-29 08:13:25 +0530

    Added Contact page and Error page

---
+++ apps/zenly/src/app/(public)/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact(){
+    return(
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '3rem', fontWeight: 'bold' }}>
+            Coming Soon
+        </div>
+    )
+}
--- apps/zenly/src/components/full-components/timeline.tsx
+++ apps/zenly/src/components/full-components/timeline.tsx
@@ -56,28 +56,28 @@ export default function TimelineDemo() {
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="/resume-builder.jpg"
               alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-contain  h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/resume-by-user.jpg"
               alt="feature template"
               width={500}
               height={500}
               className="rounded-lg object-contain  h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
+              src="/resume-opt.jpg"
               alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-contain  h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/cards.png"
+              src="/resume.jpg"
               alt="cards template"
               width={500}
               height={500}
@@ -113,14 +113,14 @@ export default function TimelineDemo() {
           </div>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://assets.aceternity.com/pro/hero-sections.png"
+              src="/features.png"
               alt="hero template"
               width={500}
               height={500}
               className="rounded-lg object-contain h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/features-section.png"
+              src="/team.png"
               alt="feature template"
               width={500}
               height={500}
